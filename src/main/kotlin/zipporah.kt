c718e09ab07603317c16b0a9225cc680607a9a39
WairimuNganga <wairimuznganga@gmail.com>
2022-02-12 17:52:39 +0300

    first commit

---
+++ src/main/kotlin/zipporah.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var name: String= "Zipporah Wairimu"
+    var age: String= "19Years"
+    var phonenumber: String= "0793935685"
+    var weightinKg: Int=60
+    var Kenyan: Boolean= true
+    println(name)
+    println(age)
+    println(phonenumber)
+    println(weightinKg)
+    println(Kenyan)
+}
+
